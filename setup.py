b55b6f83ad4fcd3a2cee090751c5d602007a7b26
sli231 <72546891+sli231@users.noreply.github.com>
2024-03-10 16:44:51 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.rst", "r", encoding="utf-8") as f:
     __long_description__ = f.read()
 
 setup(
-    name = "STALOC",
+    name = "STALocator",
     version = "1.0.0",
-    description = "Spatial transcriptomics-assisted localization for single-cell transcriptomics with STALOC",
-    url = "https://github.com/Zhanglabtools/STALOC",
+    description = "Spatial Transcriptomics-Aided Localization for Single-Cell Transcriptomics with STALocator",
+    url = "https://github.com/Zhanglabtools/STALocator",
     author = "Shang Li",
     author_email = "lishang@amss.ac.cn",
     license = "MIT",
-    packages = ['STALOC'],
+    packages = ['STALocator'],
     install_requires = ["requests",],
     zip_safe = False,
     include_package_data = True,
